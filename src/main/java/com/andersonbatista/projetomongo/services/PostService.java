91d72e8500f2d624bd3d16e3dccd844f9bde8087
Anderson Batista Ferreira da Silva <andersonbfs15@gmail.com>
2021-03-03 08:35:39 -0300

    Obtendo um post por id

---
+++ src/main/java/com/andersonbatista/projetomongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.andersonbatista.projetomongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andersonbatista.projetomongo.domain.Post;
+import com.andersonbatista.projetomongo.repository.PostRepository;
+import com.andersonbatista.projetomongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
+	
+}
