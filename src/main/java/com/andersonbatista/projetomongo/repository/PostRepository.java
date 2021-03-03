216aba2fd8035f54538d62897a00cbee6470eabb
Anderson Batista Ferreira da Silva <andersonbfs15@gmail.com>
2021-03-03 09:33:24 -0300

    Consulta Simples com @Query

--- src/main/java/com/andersonbatista/projetomongo/repository/PostRepository.java
+++ src/main/java/com/andersonbatista/projetomongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.andersonbatista.projetomongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.andersonbatista.projetomongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text); // montando consultas com QueryMethods
 }
