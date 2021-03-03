bca9dff7fa8cd3c90b3383928649227af0425ad3
Anderson Batista Ferreira da Silva <andersonbfs15@gmail.com>
2021-03-02 22:28:57 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/andersonbatista/projetomongo/config/Instantiation.java
+++ src/main/java/com/andersonbatista/projetomongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.andersonbatista.projetomongo.domain.Post;
 import com.andersonbatista.projetomongo.domain.User;
+import com.andersonbatista.projetomongo.dto.AuthorDTO;
 import com.andersonbatista.projetomongo.repository.PostRepository;
 import com.andersonbatista.projetomongo.repository.UserRepository;
 
@@ -35,10 +36,15 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("02/03/2021"), "Partiu Viagem", "Vou viajar para São Paulo!", maria);
-		Post post2 = new Post(null, sdf.parse("03/03/2021"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("02/03/2021"), "Partiu Viagem", "Vou viajar para São Paulo!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("03/03/2021"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}	
 }
+
+
+
+
